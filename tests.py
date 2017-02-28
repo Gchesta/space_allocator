3d5b91607596db308d20a3a8788f1a7f8f2eabf5
Governor <elimushwari@gmail.com>
2017-02-28 10:45:31 +0300

    Add test for raising error on duplicate value

--- tests.py
+++ tests.py
@@ -10,10 +10,10 @@ class TestCreateRoom(unittest.TestCase):
         new_room_count = len(room.all_rooms())
         self.assertTrue("Spooky" in room.all_rooms() and new_room_count - initial_room_count == 1 )
 
-    """def test_raise_eeror_on_duplicate_room(self):
+    def test_raise_eeror_on_duplicate_room(self):
     	with self.assertRaises(ValueError):
     		room_2 = Room("Office", "Spooky")
-    		room_2.create_room()"""
+    		room_2.create_room()
 
 
 
