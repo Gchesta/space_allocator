e0251ff1512ab406056b31243a2417e4a1c4f154
Governor <elimushwari@gmail.com>
2017-02-28 14:58:34 +0300

    Add tests to add new members

--- tests.py
+++ tests.py
@@ -1,23 +1,33 @@
 import unittest
-from models import Room
+from models import Dojo, Room, Office, LivingSpace, Person, Fellow
 
-class TestCreateRoom(unittest.TestCase):
+class TestRooms(unittest.TestCase):
 
     def test_create_room_successfully(self):
-        room = Room("Office", "Spooky")
-        initial_room_count = len(room.all_rooms())
-        room.create_room()
-        new_room_count = len(room.all_rooms())
-        self.assertTrue("Spooky" in room.all_rooms() and new_room_count - initial_room_count == 1 )
+        office = Office("Office", "Spooky")
+        initial_room_count = len(office.all_rooms())
+        office.create_room()
+        new_room_count = len(office.all_rooms())
+        self.assertTrue("Spooky" in office.all_rooms() and new_room_count - initial_room_count == 1 )
 
     def test_raise_eeror_on_duplicate_room(self):
     	with self.assertRaises(ValueError):
-    		room_2 = Room("Office", "Spooky")
-    		room_2.create_room()
+    		office_2 = Office("Office", "Speedy")
+    		office_2.create_room()
 
+class TestPersons(unittest.TestCase):
 
+    def test_add_new_member_successfully(self):
+        fellow = Fellow("Fellow", "George Bush")
+        initial_members_count = len(fellow.all_members())
+        fellow.add_new_member()
+        new_members_count = len(fellow.all_members())
+        self.assertTrue("George Bush" in fellow.all_members() and new_members_count - initial_members_count == 1 )
 
-
+    def test_raise_eeror_on_duplicate_persons(self):
+    	with self.assertRaises(ValueError):
+    		fellow_2 = Fellow("Fellow", "Jonathan Swift")
+    		fellow_2.add_new_member()
 
 if __name__ == "__main__":
     unittest.main()
