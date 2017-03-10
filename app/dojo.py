66bbab9eadfe77212cfd3333fd0a2852bc6b031f
Governor <elimushwari@gmail.com>
2017-03-10 18:28:57 +0300

    Refactor code to take into account reviews

--- app/dojo.py
+++ app/dojo.py
@@ -1,4 +1,6 @@
 
+from os.path import isfile, getsize
+
 from termcolor import cprint
 
 from .room import LivingSpace, Office
@@ -9,17 +11,13 @@ from random import choice
 class Dojo:
     """
     A class that contains the methods to create a new room and to add new persons to the
-    Dojo.
-
-    """
+    Dojo."""
 
     def __init__(self):
-
         self.rooms = []
         self.persons = []
 
     def create_room(self, name, category):
-
         name = str(name).capitalize()
         category = str(category).capitalize()
         room_is_invalid = self.check_room_invalidity(name, category)
@@ -48,20 +46,20 @@ class Dojo:
             return ("\n room name contains"
              "non-alphabets\nRoom not added\n")
 
-        room_already_exists = self.check_if_room_exists(name)
+        room_exists = self.check_if_room_exists(name)
 
-        if room_already_exists:
+        if room_exists != "No such room exists":
             return("\n The name '%s' already exists\n"
                 "Room not added\n" % name)
         else:
             return False
 
-    def check_if_room_exists(self, name):
-        for room in self.rooms:
-            if room.name == name:
-                return True
-        return False
-
+    def check_if_room_exists(self, name):        
+        try:
+            check = [room for room in self.rooms if room.name == name][0]
+        except IndexError:
+            check = "No such room exists"
+        return check
 
     def allocate_the_unallocated(self, category):
 
@@ -89,12 +87,11 @@ class Dojo:
         elif person.office == "Pending":
             person.office = "Unallocated"
             cprint("\n %s has not been allocated to"
-                 "an office" % (str(person)), "magenta")
+                 " an office" % (str(person)), "magenta")
         else:
             pass
 
     def allocate_livingspace(self, person):
-
         available_livingspace = self.check_room_availability("Living Space")        
 
         if available_livingspace:
@@ -111,22 +108,19 @@ class Dojo:
         else:
             pass
         
-
     def check_room_availability(self, category):
         try:
             available_room = choice(
                     [room for room in self.rooms if room.category == category and room.available_capacity])
         except IndexError:
             available_room = False
-
         return available_room
 
     def add_person(self, category, name, surname, accomodation="N"):
-
         name = str(name).capitalize() + " " + str(surname).capitalize()
-        accomodation = accomodation.capitalize()
-        category = category.capitalize()
-        person_is_invalid = self.check_person_invalidity(name, accomodation)
+        accomodation = str(accomodation).capitalize() if accomodation else "N"
+        category = str(category).capitalize()
+        person_is_invalid = self.check_person_invalidity(name, accomodation, category)
 
         if person_is_invalid:
             cprint(person_is_invalid, "magenta")
@@ -142,7 +136,6 @@ class Dojo:
         else:
             person = Staff(name, office)
 
-        person.idno = len(self.persons) + 1    
         self.persons.append(person)
         cprint("\n %s has been added to the Dojo as a %s" % (name, category), "green")
         self.allocate_office(person)
@@ -150,9 +143,9 @@ class Dojo:
         if accomodation == "Pending":
             self.allocate_livingspace(person)
 
-    def check_person_invalidity(self, name, accomodation="N"):
+    def check_person_invalidity(self, name, accomodation, category):
         name_split = name.split(" ")
-
+        
         if not name_split[0].isalpha() or not name_split[1].isalpha():
             return ("\n %s contains non-alphabets\n"
                 "Person not added\n" % name)
@@ -167,113 +160,98 @@ class Dojo:
         if accomodation not in ("N","Y"):
             return ("Choose either 'Y' or 'N' for accomodation")
 
+        if category not in ("Fellow", "Staff"):
+            return "Invalid person category"
+
         return False
 
     def print_room(self, name):
         name = name.capitalize()
-        room_exists = self.check_if_room_exists(name)
-        if room_exists:
-            for room in self.rooms:
-                if room.name == name:
-                    cprint("\nROOM NAME: %s\tROLE\n" % name.upper(), "yellow")
-                    for occupant in room.occupants:
-                        print(str(occupant) + " \t\t" + occupant.category.upper())
-
+        room = self.check_if_room_exists(name)
+        if room != "No such room exists":
+            cprint("\nROOM NAME: %s\tROLE\n" % name.upper(), "yellow")
+            for occupant in room.occupants:
+                print(str(occupant) + " \t\t" + occupant.category.upper())
         else:
-            cprint("Room does not exists", "magenta")
-
-    def choose_headings(self, origin):
-        #headings and sub_headings for print_allocations
-        heading_allocations_offices = "\nALLOCATIONS - OFFICES"
-        subheading_allocations_offices = "\nROOM NAME: %s\tROLE\t\tACCOMODATION\n"
-        heading_allocations_livingspaces = "\nALLOCATIONS - LIVING SPACES"
-        sub_heading_allocations_livingspaces = "\nROOM NAME: %s\tROLE\t\tOFFICE\n"
-
-        if origin == "print_allocations":
-            return(heading_allocations_offices, subheading_allocations_offices,
-                heading_allocations_livingspaces, sub_heading_allocations_livingspaces)
-
-        #if origin is not print_allocations, it is print_unallocated
-        #headings and sub_headings for print_unallocations.
-        heading_unallocated_offices = "\nUNALLOCATED - OFFICES"
-        heading_unallocated_livingspaces = "\nUNALLOCATED - LIVING SPACES"
-        sub_heading_unallocated = "\nPERSON NAME\t\tROLE\n"
-
-        return(heading_unallocated_offices, heading_unallocated_livingspaces,
-         sub_heading_unallocated)
+            cprint(room, "magenta")
 
     def print_allocations(self, filename=""):
-        headings = self.choose_headings("print_allocations")
-        heading_office = headings[0]
-        sub_heading_office = headings[1]
-        heading_livingspace = headings[2]
-        sub_heading_livingspace = headings[3]
+        heading_office = "\nALLOCATIONS - OFFICES"
+        sub_heading_office = "\nROOM NAME: %s\tROLE\t\tACCOMODATION\n"
+        heading_livingspace = "\nALLOCATIONS - LIVING SPACES"
+        sub_heading_livingspace = "\nROOM NAME: %s\tROLE\t\tOFFICE\n"
+        
+        allocations = self.get_allocations()
+        offices = allocations[0]
+        livingspaces = allocations[1]
 
-        cprint(heading_office,  "blue")
-        for room in self.rooms:
-            if room.category == "Office":
-                cprint(sub_heading_office % room.name.upper(), "yellow")
-                for occupant in room.occupants:
-                    print("%s \t\t%s\t\t%s" %(str(occupant), 
-                        occupant.category.upper(), str(occupant.accomodation)))
+        cprint(heading_office, "blue")
+        for room, occupants in offices.items():
+            cprint(sub_heading_office % room.name.upper(),  "yellow")
+            print(occupants)
 
         cprint(heading_livingspace, "blue")
-        for room in self.rooms:
-            if room.category == "Living Space":
-                cprint(sub_heading_livingspace % room.name.upper(),  "yellow")
-                for occupant in room.occupants:
-                    print("%s \t\t%s\t\t%s" %(str(occupant), 
-                        occupant.category.upper(), str(occupant.office)))
+        for room, occupants in livingspaces.items():
+            cprint(sub_heading_livingspace % room.name.upper(),  "yellow")
+            print(occupants)
                             
         if filename:
             with open(filename, "w") as outputfile:
                 outputfile.write(heading_office)
-                for room in self.rooms:
-                    if room.category == "Office":
-                        outputfile.write(sub_heading_office % room.name.upper())
-                        for occupant in room.occupants:
-                            outputfile.write("%s \t\t%s\t\t%s\n" %(str(occupant), 
-                        occupant.category.upper(), str(occupant.accomodation)))
-
+                for room, occupants in offices.items():
+                     outputfile.write(sub_heading_office % room.name.upper())
+                     outputfile.write(occupants)
                 outputfile.write(heading_livingspace)
-                for room in self.rooms:
-                    if room.category == "Living Space":
-                        outputfile.write(sub_heading_livingspace % room.name.upper())
-                        for occupant in room.occupants:
-                            outputfile.write("%s \t\t%s\t\t%s\n" % (str(occupant), 
-                        occupant.category.upper(), str(occupant.office)))
-
+                for room, occupants in livingspaces.items():
+                    outputfile.write(sub_heading_livingspace % room.name.upper())
+                    outputfile.write(occupants)
 
     def print_unallocated(self, filename=""):
-        headings = self.choose_headings("print_unallocated")
-        heading_office = headings[0]
-        heading_livingspace = headings[1]
-        sub_heading = headings[2]
+        heading_office = "\nUNALLOCATED - OFFICES"
+        heading_livingspace = "\nUNALLOCATED - LIVING SPACES"
+        sub_heading = "\nPERSON NAME\t\tROLE\n"
+        unallocated_livingspace = ""
+        unallocated_office = ""
+
+        for person in self.persons:
+            if person.office == "Unallocated":
+                unallocated_office += ("%s\t\t%s\n" % (person.name, person.category.upper()))
+
+            if person.accomodation == "Unallocated":
+                unallocated_livingspace += ("%s\t\t%s\n" % (person.name, person.category.upper()))
 
         cprint(heading_office,  "blue")
         cprint(sub_heading,  "yellow")
-        for person in self.persons:
-            if person.office == "Unallocated":
-                print("%s\t\t%s" % (person.name, person.category.upper()))
+        print(unallocated_office)
 
         cprint(heading_livingspace,  "blue")
         cprint(sub_heading,  "yellow")
-        for person in self.persons:
-            if person.accomodation == "Unallocated":
-                print("%s\t\t%s" % (person.name, person.category.upper()))
+        print(unallocated_livingspace)
 
         if filename:
             with open(filename, "w") as outputfile:
-                outputfile.write(heading_office + "\n")
-                outputfile.write(sub_heading)
-                for person in self.persons:
-                    if person.office == "Unallocated":
-                        outputfile.write("%s\t\t%s\n" %(person.name, person.category.upper()))
+                outputfile.write("%s\n%s" %(heading_office, sub_heading))
+                outputfile.write(unallocated_office)
+                outputfile.write("%s\n%s" %(heading_livingspace, sub_heading))
+                outputfile.write(unallocated_livingspace)
 
-                outputfile.write(heading_livingspace)
-                outputfile.write(sub_heading)
-                for person in self.persons:
-                    if person.accomodation == "Unallocated":
-                        outputfile.write("%s\t\t%s\n" % (person.name, person.category.upper()))
+    def get_allocations(self):
+        offices = {}
+        livingspaces = {}
+        for room in self.rooms:
+            if room.category == "Office":
+                allocations_office = ""
+                for occupant in room.occupants:
+                     allocations_office += ("%s \t\t%s\t\t%s\n" %(str(occupant), 
+                        occupant.category.upper(), str(occupant.accomodation)))
+                offices[room] = allocations_office
+            else:
+                allocations_livingspace = ""
+                for occupant in room.occupants:
+                    allocations_livingspace += ("%s \t\t%s\t\t%s\n" %(str(occupant), 
+                        occupant.category.upper(), str(occupant.office)))
+                livingspaces[room] = allocations_livingspace
+        return(offices, livingspaces)
+
 
 
